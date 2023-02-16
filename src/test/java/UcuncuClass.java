9576d535b23bb262b5683946b57b3767122a9d0a
OmerTUFAN <98853013+OTufan1@users.noreply.github.com>
2023-02-16 01:49:58 +0000

    Update UcuncuClass.java

--- src/test/java/UcuncuClass.java
+++ src/test/java/UcuncuClass.java
@@ -1,5 +1,6 @@
 public class UcuncuClass {
     public static void main(String[] args) {
         System.out.println("Hello motherfuckers, dady is home...");
+        System.out.println("Github'ta yaptigim degisiklik");
     }
 }
