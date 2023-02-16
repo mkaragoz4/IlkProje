c6757eebeb9a174bb2817a80f20cb1bda8f67dc1
M.KARAGÖZ <78112216+mkaragoz4@users.noreply.github.com>
2023-02-16 02:53:02 +0100

    Update UcuncuClass.java

--- src/test/java/UcuncuClass.java
+++ src/test/java/UcuncuClass.java
@@ -2,6 +2,6 @@ public class UcuncuClass {
     public static void main(String[] args) {
         System.out.println("Hello motherfuckers, dady is home...");
         System.out.println("Github'ta yaptigim degisiklik");
-        System.out.println("vay aliminyum")
+        System.out.println("vay aliminyum");
     }
 }
